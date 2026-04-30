5b02102f21d6dc871b59ffe170a798783545735b
Rae <raeouyang@RaeOuyang.local>
2026-04-30 18:27:49 +0800

    Bump cached module versions

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 import { applySiteConfig, bindHeroIntroVideoSource } from './site-config.js?v=202604287';
 import { initFeatureCarousel } from './feature-carousel.js?v=202604302';
-import { initFloatingForm } from './floating-form.js?v=202604287';
+import { initFloatingForm } from './floating-form.js?v=202604304';
 import { initHeroVideoModal } from './hero-video.js?v=202604287';
 import { loadWebsiteBottomConfig } from './footer-config.js?v=202604304';
 import { bindSupplierReveal } from './supplier-reveal.js?v=202604287';
